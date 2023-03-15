568f2b8be66e7c842fe70e8ac6bd6d8df32f5bae
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-16 02:14:10 +0530

    Update todo.js

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,7 +1,12 @@
 const todoReducer = (state, action) => {
-    switch (action.type) {
+	switch (action.type) {
+		case 'addItem':
+			return [...state, action.title];
+		case 'removeItem':
+			return state.filter((_, idx) => idx !== action.payload);
+		default:
+			return new Error();
+	}
+};
 
-    }
-}
-
-export { todoReducer }
+export { todoReducer };
