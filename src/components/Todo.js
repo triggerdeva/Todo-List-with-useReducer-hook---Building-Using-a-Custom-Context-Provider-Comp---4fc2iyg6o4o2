04538c0f2ce29a252a135843edcdc6d94de6374c
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-16 02:13:32 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,20 @@
 import React from 'react';
-const Todo = () => {
-    return (
-        <></>
-    )
-}
+const Todo = ({ item, id, dispatch }) => {
+	return (
+		<div className="todo" id={id}>
+			<div className="todo-title">{item}</div>
+			<button
+				className="todo-delete"
+				onClick={() => {
+					dispatch({
+						type: 'removeItem',
+						payload: id,
+					});
+				}}>
+				Remove
+			</button>
+		</div>
+	);
+};
 
-
-export { Todo }
-
+export { Todo };
